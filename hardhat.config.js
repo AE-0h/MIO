bd830c81f1c6cadd0137b83392b880dfb5e86a15
Conor Gallagher <dontlisten33@gmail.com>
2023-01-19 00:53:47 -0800

    HARDHAT: configured to use foundry

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,6 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("@nomicfoundation/hardhat-chai-matchers");
+require("@nomicfoundation/hardhat-foundry");
 
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
