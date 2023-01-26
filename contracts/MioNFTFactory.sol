130bc03aab1238364f2f7360fac0ef250e3a12b4
Conor Gallagher <dontlisten33@gmail.com>
2023-01-25 18:26:23 -0800

    SOLIDITY: CLEAN UP FACTORY AND CORE

--- contracts/MioNFTFactory.sol
+++ contracts/MioNFTFactory.sol
@@ -5,19 +5,15 @@ import {MioNFT} from "./MioNFT.sol";
 import {Owned} from "solmate/src/auth/Owned.sol";
 import {MIOCore} from "./MioCore.sol";
 
-
-
 contract MioNFTFactory{
-
-
-
+//--------------------------------------------ERRORS--------------------------------------------------------
      error NotMioCore();
-
+//------------------------------------------IMMUTABLES------------------------------------------------------
      MioNFT immutable public mioNFT;
      MIOCore immutable public mioCore;
-
+//--------------------------------------------Events--------------------------------------------------------
     event ContractDeployed(address contractAddress);
-
+//------------------------------------------CONSTRUCTOR-----------------------------------------------------
     constructor(
         MioNFT _mioNFT,
         MIOCore _mioCore
@@ -25,7 +21,7 @@ contract MioNFTFactory{
         mioNFT = _mioNFT;
         mioCore = _mioCore;
     }
-
+//-------------------------------------------FUNCTIONS------------------------------------------------------
        function deployUserContract(string memory _name, string memory _symbol ) external returns (address)  {
         if(msg.sender != address(mioCore)) { revert NotMioCore(); }
         address newMioNFTContract = address( new MioNFT(_name, _symbol));
--- hardhat.config.js
+++ hardhat.config.js
@@ -2,14 +2,6 @@ require("@nomicfoundation/hardhat-toolbox");
 require("@nomicfoundation/hardhat-chai-matchers");
 require("@nomicfoundation/hardhat-foundry");
 
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
