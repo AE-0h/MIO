d25d4248d54d128b9cafad61451b2dd6c1fe516b
Conor Gallagher <dontlisten33@gmail.com>
2023-05-10 10:56:56 -0700

    HARDHAT CONFIG: update RPC endpoints

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,11 +6,10 @@ require("@nomiclabs/hardhat-etherscan");
 require("@nomiclabs/hardhat-ethers");
 require("dotenv").config();
 
-const ALCHEMY_API_KEY = process.env.NEXT_PUBLIC_ALCHEMY_ID;
-const AlCHEMY_API_KEY_GOERLI = process.env.NEXT_PUBLIC_ALCHEMY_ID_GOERLI;
+const ALCHEMY_API_KEY = process.env.NEXT_PUBLIC_ALCHEMY_ID_SEPOLIA;
+const QUICKNODE_API_KEY_GOERLI = process.env.NEXT_PUBLIC_QUICKNODE_GOERLI;
 const PRIVATE_KEY = process.env.NEXT_PUBLIC_PRIVATE_KEY;
 const SECOND_PRIVATE_KEY = process.env.NEXT_PUBLIC_PRIVATE_KEY_TWO;
-const ALCHEMY_API_KEY_MAINNET = process.env.NEXT_PUBLIC_ALCHEMY_MAINNET_ID;
 const ETHERSCAN_KEY = process.env.NEXT_PUBLIC_ETHERSCAN_KEY;
 const INFURA_WEB3_KEY = process.env.NEXT_PUBLIC_INFURA_KEY;
 
@@ -23,15 +22,15 @@ module.exports = {
     mumbai: {
       url: `https://polygon-mumbai.infura.io/v3/${INFURA_WEB3_KEY}`,
       accounts: [PRIVATE_KEY, SECOND_PRIVATE_KEY],
-      gasPrice: 900000000,
+      gasPrice: 10000000000,
       gasLimit: 500000,
     },
-    polygon: {
-      url: `https://polygon-mainnet.g.alchemy.com/v2/${ALCHEMY_API_KEY_MAINNET}`,
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
       accounts: [PRIVATE_KEY],
     },
     goerli: {
-      url: `https://ancient-shy-gas.ethereum-goerli.discover.quiknode.pro/a8428ff11043166324f36a939ab75254969c5bbe/`,
+      url: `https://ancient-shy-gas.ethereum-goerli.discover.quiknode.pro/${QUICKNODE_API_KEY_GOERLI}/`,
       accounts: [PRIVATE_KEY, SECOND_PRIVATE_KEY],
     },
   },
